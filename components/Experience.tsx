356d3878ba228e0140ef014a13c47b4011b46a00
Adesemi Elumaro <adesemi.elumaro@duke.edu>
2022-09-24 20:53:05 +0100

    setup other sections

---
+++ components/Experience.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Experience = (props: Props) => {
+  return <div className="h-screen">Experience</div>;
+};
+
+export default Experience;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Experience from "../components/Experience";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 const Home: NextPage = () => {
   return (
@@ -11,15 +17,26 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
-      <section id="about"></section>
-      <section id="skills"></section>
-      <section id="projects"></section>
-      <section id="experience"></section>
-      <section id="contact"></section>
+      <section id="about" className="snap-center">
+        <About />
+      </section>
+      <section id="skills" className="snap-center">
+        <Skills />
+      </section>
+      <section id="projects" className="snap-center">
+        <Projects />
+      </section>
+      <section id="experience" className="snap-center">
+        <Experience />
+      </section>
+      <section id="contact" className="snap-end">
+        <Contact />
+      </section>
+
+      <Footer />
     </div>
   );
 };
